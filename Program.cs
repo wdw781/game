3719aa9c20e009a12d4caf146c11fd59dfa18046
wdw <wernercancel018@gmail.com>
2022-10-15 06:03:30 +0800

    coo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace My_Awesome_Program
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Title = "owo";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
